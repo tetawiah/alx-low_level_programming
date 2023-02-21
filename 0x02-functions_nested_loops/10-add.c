63507b3ce6a26d1d32a701a90599beaf45272a0c
etawia <tawiah.tetteh10@gmail.com>
2023-02-21 15:33:24 -0800

    addc

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @num1: first int
+ * @num2: second int
+ *
+ * Return: The result. 
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
