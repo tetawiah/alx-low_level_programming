3bc72999677450db1d2ecbb950b937a746337157
etawia <tawiah.tetteh10@gmail.com>
2023-02-21 15:14:15 -0800

    5sign

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs -will computes the absolute value of integer passed.
+ * @n: passed integer to be computed.
+ *
+ * Return: The absolute value of the integer.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
