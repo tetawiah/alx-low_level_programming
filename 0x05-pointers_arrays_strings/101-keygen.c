ab22c51457cc20698689d031842e8fd1f73f7489
etawia <tawiah.tetteh10@gmail.com>
2023-02-28 15:27:49 -0800

    101keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
@@ -9,45 +10,45 @@
  */
 int main(void)
 {
-  char password[84];
-  int index = 0, sum = 0, diff_half1, diff_half2;
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
-  srand(time(0));
+	srand(time(0));
 
-  while (sum < 2772)
-    {
-      password[index] = 33 + rand() % 94;
-      sum += password[index++];
-    }
-
-  password[index] = '\0';
-
-  if (sum != 2772)
-    {
-      diff_half1 = (sum - 2772) / 2;
-      diff_half2 = (sum - 2772) / 2;
-      if ((sum - 2772) % 2 != 0)
-	diff_half1++;
-
-      for (index = 0; password[index]; index++)
+	while (sum < 2772)
 	{
-	  if (password[index] >= (33 + diff_half1))
-	    {
-	      password[index] -= diff_half1;
-	      break;
-	    }
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
 	}
-      for (index = 0; password[index]; index++)
+
+	password[index] = '\0';
+
+	if (sum != 2772)
 	{
-	  if (password[index] >= (33 + diff_half2))
-	    {
-	      password[index] -= diff_half2;
-	      break;
-	    }
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half1))
+			{
+				password[index] -= diff_half1;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
+		}
 	}
-    }
 
-  printf("%s", password);
+	printf("%s", password);
 
-  return (0);
+	return (0);
 }
